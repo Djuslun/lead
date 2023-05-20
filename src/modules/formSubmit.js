172eef896c022cc9ad7c75fac8580b36b5671104
Djuslun <djuslun@yandex.ru>
2023-05-20 21:40:41 +0300

    add form submit

--- src/modules/formSubmit.js
+++ src/modules/formSubmit.js
@@ -1,8 +1,38 @@
+import { showPopUp } from "./showPopUp";
+
 const footerForm = document.querySelector('.footer__form');
 
-footerForm.addEventListener('submit', (event) => {
+footerForm.addEventListener('submit', async (event) => {
   event.preventDefault()
-  console.log('++')
-  popUp.classList.add('pop-up--active')
-  document.body.classList.add('body--active')
+
+  let formDataObject = {};
+  const formData = new FormData(footerForm);
+
+  formData.forEach(function (value, key) {
+    formDataObject[key] = value;
+  });
+
+  try {
+    const response = await fetch('https://kind-jay-tank-top.cyclic.app/api', {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json; charset=utf-8'
+      },
+      body: JSON.stringify(formDataObject)
+    })
+
+    let result = await response.json();
+
+    if (response.ok) {
+      showPopUp('ok')
+      console.log(result)
+    } else {
+      throw new Error('Submit error')
+    }
+  } catch (e) {
+    showPopUp('error')
+    console.log(e)
+  } finally {
+    footerForm.reset();
+  }
 })
--- src/modules/showPopUp.js
+++ src/modules/showPopUp.js
@@ -1,9 +1,24 @@
 const popUp = document.querySelector('.pop-up')
 const popUpCloseButtons = document.querySelectorAll('.close')
+const popUpTitle = document.querySelector('.pop-up__title')
+const popUpDescription = document.querySelector('.pop-up__description')
 
-popUpCloseButtons.forEach(item => {
-  item.addEventListener('click', () => {
-    popUp.classList.remove('pop-up--active')
-    document.body.classList.remove('body--active')
-  })
-})
+export const showPopUp = (status) => {
+  if (status === 'error') {
+    popUpTitle.textContent = 'ERROR!'
+    popUpDescription.textContent = "Something goes wrong("
+  }
+  if (status === 'ok') {
+    popUpTitle.textContent = 'SUCCESS!'
+    popUpDescription.textContent = "You have successfully subscribed to the email newsletter"
+  }
+  popUp.classList.add('pop-up--active')
+  document.body.classList.add('body--active')
+}
+
+const hidePopUp = () => {
+  popUp.classList.remove('pop-up--active')
+  document.body.classList.remove('body--active')
+}
+
+popUpCloseButtons.forEach(item => item.addEventListener('click', hidePopUp))
